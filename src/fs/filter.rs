564dc3b8015c8b1d05b324bb086abd6b21381084
Joey Sabey <joey.sabey@gmx.com>
2024-03-16 12:39:42 +0000

    crude short-circuit extension based 'file' sorting to ignore directories

--- src/fs/filter.rs
+++ src/fs/filter.rs
@@ -255,14 +255,24 @@ impl SortField {
                 order            => order,
             },
 
-            Self::Extension(ABCabc) => match a.ext.cmp(&b.ext) {
-                Ordering::Equal  => natord::compare(&a.name, &b.name),
-                order            => order,
+            Self::Extension(ABCabc) => {
+                if a.is_directory() || b.is_directory() {
+                    return Ordering::Equal;
+                }
+                match a.ext.cmp(&b.ext) {
+                    Ordering::Equal  => natord::compare(&a.name, &b.name),
+                    order            => order,
+                }
             },
 
-            Self::Extension(AaBbCc) => match a.ext.cmp(&b.ext) {
-                Ordering::Equal  => natord::compare_ignore_case(&a.name, &b.name),
-                order            => order,
+            Self::Extension(AaBbCc) => {
+                if a.is_directory() || b.is_directory() {
+                    return Ordering::Equal;
+                }
+                match a.ext.cmp(&b.ext) {
+                    Ordering::Equal  => natord::compare_ignore_case(&a.name, &b.name),
+                    order            => order,
+                }
             },
 
             Self::NameMixHidden(ABCabc) => natord::compare(
